7d8756c7073544c57de7e0ab97d92c6003633a37
NimaShariatz <shariatz@ualberta.ca>
2025-04-20 22:39:34 -0600

    part2 work and menu setup

--- igem assignment/src/marl_club/marl_club.jsx
+++ igem assignment/src/marl_club/marl_club.jsx
@@ -1,3 +1,5 @@
+import './marl_club.css'
+import logo from '../assets/imgs/CAL_logo.png'
 
 
 
@@ -9,8 +11,44 @@ function Marl_club() {
 
     return(
         
-        <div>
-            <p>Hello</p>
+        <div className="menu_container">
+            <ul>
+
+                <li>
+                    <div>
+                        <img className='menu_logo' src={logo} alt="Classic Autosport Liveries Logo"/>
+                    </div>
+                </li>
+
+
+                <li className='has_submenu'>
+                    <p>MARLBORO CLUB</p>
+                    <div className='submenu_container'>
+
+                    </div>
+                </li>
+
+                <li>
+                    <p>QUATTRO CLUB</p>
+                </li>
+
+                <li>
+                    <p>MARTINI CLUB</p>
+                </li>
+
+                <li>
+                    <p>JPS CLUB</p>
+                </li>
+
+                <li>
+                    <p>GULF CLUB</p>
+                </li>
+
+            </ul>
+
+
+            
+
         </div>
 
 
