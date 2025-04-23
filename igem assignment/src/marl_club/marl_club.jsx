7ca888d712a519cceb63ff6cedbee42aa9f6713a
NimaShariatz <shariatz@ualberta.ca>
2025-04-22 20:56:16 -0600

    part 2 menu done

--- igem assignment/src/marl_club/marl_club.jsx
+++ igem assignment/src/marl_club/marl_club.jsx
@@ -10,76 +10,93 @@ import Component from './component'
 function Marl_club() {
 
     return(
+        <div>
         
-        <div className="menu_container">
-            <ul>
+            <div className="menu_container">
+                <ul>
 
-                <li>
-                    <div>
-                        <img className='menu_logo' src={logo} alt="Classic Autosport Liveries Logo"/>
-                    </div>
-                </li>
+                    <li>
+                        <div>
+                            <img className='menu_logo' src={logo} alt="Classic Autosport Liveries Logo"/>
+                        </div>
+                    </li>
 
 
-                <li className='has_submenu'>
-                    <p>MARLBORO CLUB</p>
-                    <div className='submenu_container'>
+                    <li className='has_submenu'>
+                        <p className='club'>MARLBORO CLUB</p>
+                        <div className='submenu_container'>
 
-                        <div className='submenu_flex'>
-                            <div className='left_section'>
-                                
-                                <div className='left_section_column'>
-                                    <div className='menu_option'>
-                                        <h3>Overview</h3>
-                                        <p>An retrospective of the Marlboro brand in various forms of motorsport throughout the decades</p>
+                            <div className='submenu_flex'>
+                                <div className='left_section'>
+                                    
+                                    <div className='section_column'>
+                                        <div className='menu_option'>
+                                            <h3>Overview</h3>
+                                            <p>An retrospective of the Marlboro brand in various forms of motorsport throughout the decades</p>
+                                        </div>
+                                        <div className='menu_option'>
+                                            <h3>Formula One</h3>
+                                            <p>A look at Marlboro sponsorship during the V8, V12, and V10 era</p>
+                                        </div>
+                                        <div className='menu_option'>
+                                            <h3>WRC/Group Rally</h3>
+                                            <p>Iconic Marlboro sponsorships in WRC and Group rallying</p>
+                                        </div>
                                     </div>
-                                    <div className='menu_option'>
-                                        <h3>Formula One</h3>
-                                        <p>A look at Marlboro sponsorship during the V8, V12, and V10 era</p>
+
+
+                                </div>{/* left_section */}
+
+                                <div className='right_section'>
+                                    <div className='section_column'>
+                                        <div className='red_section'>
+                                            <div className='menu_option_red'>
+                                                <h3>Marlboro Liveries</h3>
+                                                <p>The campaign drive and international recognition</p>
+                                            </div>  
+                                            <div className='menu_option_red'>
+                                                <h3>The Tobacco Ban</h3>
+                                                <p>The final years of tobacco liveries</p>
+                                            </div>  
+                                        </div>
+
+                                        <div className='menu_option' style={{marginTop:"25px"}}>
+                                                <h3>Racing Icons</h3>
+                                                <p>The professionals with Marlboro embossing</p>
+                                        </div>  
+
                                     </div>
-                                    <div className='menu_option'>
-                                        <h3>WRC/Group Rally</h3>
-                                        <p>Iconic Marlboro sponsorships in WRC and Group rallying</p>
-                                    </div>
-                                </div>
+                                </div>{/* right_section */}
+
+                            </div>{/* submenu_flex */}
+
+                        </div>{/* submenu_container */}
+                    </li>
+
+                    <li>
+                        <p className='club'>QUATTRO CLUB</p>
+                    </li>
+
+                    <li>
+                        <p className='club'>MARTINI CLUB</p>
+                    </li>
+
+                    <li>
+                        <p className='club'>JPS CLUB</p>
+                    </li>
+
+                    <li>
+                        <p className='club'>GULF CLUB</p>
+                    </li>
+
+                </ul>
 
 
-                                <div className='left_section_column'>
+                
 
-                                </div>
-
-
-                            </div>{/* left_section */}
-
-                            <div className='right_section'>
-
-                            </div>{/* right_section */}
-
-                        </div>{/* submenu_flex */}
-
-                    </div>{/* submenu_container */}
-                </li>
-
-                <li>
-                    <p>QUATTRO CLUB</p>
-                </li>
-
-                <li>
-                    <p>MARTINI CLUB</p>
-                </li>
-
-                <li>
-                    <p>JPS CLUB</p>
-                </li>
-
-                <li>
-                    <p>GULF CLUB</p>
-                </li>
-
-            </ul>
+            </div>{/* menu */}
 
 
-            
 
         </div>
 
